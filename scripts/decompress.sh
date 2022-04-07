8b38a828b4d9d36e225dc37166243b24ccf9a302
Wout Schellaert <wout.schellaert@gmail.com>
2022-04-07 15:07:26 +0200

    Init environment

---
+++ scripts/decompress.sh
@@ -0,0 +1,16 @@
+# lrzuntar -p 1 -d BIG-bench/bigbench/benchmark_tasks/abstract_narrative_understanding/results/full-logs_BIG-G_BIG-G-sparse.tar.lrz -f -O tmp/
+decompress() {
+  path=$1
+  task=$(echo $path | cut -d'/' -f4)
+  echo "Decompressing $task"
+  mkdir -p data/$task
+  lrzuntar -q -p 1 -O data/$task/ -d $path
+}
+export -f decompress
+
+parallel \
+  -v          `# Print the job to be run on stdout` \
+  --lb        `# Output on line basis` \
+  --progress  `# Display progress ` \
+  decompress \
+  ::: $(ls BIG-bench/bigbench/benchmark_tasks/**/results/full-logs**)
