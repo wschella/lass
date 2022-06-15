973f2008070a1955f2343ab255cd16f98b64ade6
Wout Schellaert <wout.schellaert@gmail.com>
2022-06-15 13:44:15 +0200

    Add some code for setting up RNG and seeding

---
+++ src/lass/setup.py
@@ -0,0 +1,17 @@
+def setup_rng(seed: int, include_algos: bool = False) -> None:
+    import numpy as np
+    import torch
+    import random
+    import os
+
+    random.seed(seed)
+    torch.manual_seed(random.randint(1, 1_000_000))
+    torch.cuda.manual_seed(random.randint(1, 1_000_000))
+    np.random.seed(random.randint(1, 1_000_000))
+    os.environ["PYTHONHASHSEED"] = str(random.randint(1, 1_000_000))
+
+    if include_algos == True:
+        import torch.backends.cudnn
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        torch.use_deterministic_algorithms(mode=True)
