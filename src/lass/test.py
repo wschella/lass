343871e8155db80256a7d8e8632ff50c21bfe31a
Wout Schellaert <wout.schellaert@gmail.com>
2022-07-20 13:06:03 +0200

    Varia experiments and analysis

--- src/lass/test.py
+++ src/lass/test.py
@@ -6,8 +6,8 @@ from pprint import pprint
 
 from transformers.models.auto.modeling_auto import AutoModelForSequenceClassification
 from transformers.trainer import Trainer
-from transformers.models.auto.tokenization_auto import AutoTokenizer
 
+import lass.pipeline
 import lass.datasets
 import lass.metrics
 from lass.metrics.baseline import analyse, merge
@@ -29,18 +29,9 @@ def test(
     # output_dir: Optional[Union[Path, str]] = None
 ):
     logging.info("Starting data loading")
-    loader = LogLoader(
-        logdir=data_args.logdir,
-        tasks=data_args.tasks,
-        model_families=data_args.model_families,
-        model_sizes=data_args.model_sizes,
-        shots=data_args.shots,
-        query_types=data_args.query_types,
-        exclude_faulty_tasks=data_args.exclude_faulty_tasks,
-        include_unknown_shots=data_args.include_unknown_shots
-    )
+    loader = LogLoader.from_args(data_args)
     data = lass.datasets.to_dataframe(loader)
-    data['n_targets'] = data['targets'].map(lambda x: len(x))
+    data = lass.pipeline.augment(data)
     logging.info("Loaded data.")
 
     # Prepending of extra info for assessor to input
@@ -48,57 +39,29 @@ def test(
         len(data_args.model_families or []) != 1 or
         len(data_args.model_sizes or []) != 1)
 
-    model_prepender = lambda r: ""
-    if include_model_in_input and uses_pop_data:
-        model_prepender = lambda r: f"FAM: {r['model_family']} SIZE:{r['model_size']} "
+    data = lass.pipeline.prepend_extra_features(
+        data,
+        include_model=include_model_in_input,
+        include_n_targets=include_n_targets_in_input)
 
-    n_targets_prepender = lambda r: ""
-    if include_n_targets_in_input:
-        n_targets_prepender = lambda r: f"N_TARGETS: {r['n_targets']} "
-
-    prepender = lambda r: f"{model_prepender(r)} {n_targets_prepender(r)} {r.input}"
-    data['input'] = data.apply(prepender, axis=1)
-
-    # Split into train and test
-    if split == 'instance':
-        train, test = lass.datasets.split_instance_level(data, seed, test_fraction)
-    elif split == 'task':
-        train, test = lass.datasets.split_task_level(data, seed, test_fraction)
-    elif split == 'task_DS':
-        train, test = lass.datasets.split_task_level_distribution_shift(data, seed)
+    # TODO: We should binarize before the split (before augmentation)
+    train, test = lass.datasets.split(split, data, test_fraction=test_fraction, seed=seed)
+    train, test = lass.pipeline.binarize(train), lass.pipeline.binarize(test)
 
     # Log some stats & examples
     stats = merge(analyse(train), analyse(test), 'train', 'test')
     pprint(stats)
     print(train.head(1))
 
-    # Huggingfaceify
     dataset = lass.datasets.huggingfaceify_splits(train, test)
     print(dataset['train'][0])
 
     # Tokenize dataset
     logging.info("Starting tokenization")
     os.environ['TOKENIZERS_PARALLELISM'] = "true"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    if model_name == "gpt2":
-        tokenizer.pad_token = tokenizer.eos_token
-
-    def tokenize_function(examples):
-        return tokenizer(
-            examples["text"],
-            padding="max_length",
-            truncation=True,
-            max_length=max_sequence_length,
-            return_tensors="np"
-        )
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
-
-    train_dataset = tokenized_datasets["train"].shuffle(seed=seed)
-    eval_dataset = tokenized_datasets["test"]
-    len(train_dataset), len(eval_dataset)
+    tokenized_datasets = lass.pipeline.tokenize(dataset, model_name, max_sequence_length)
 
     model = AutoModelForSequenceClassification.from_pretrained(model_loc, num_labels=2)
-
     compute_metrics = lass.metrics.hf.get_metric_computer([
         "accuracy",
         "precision",
--- utils/run1.py
+++ utils/run1.py
@@ -5,7 +5,8 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
-from lass.train import train, DataArgs
+from lass.train import train
+from lass.log_handling import LoaderArgs
 # autopep8: on
 
 
@@ -16,29 +17,32 @@ def main():
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     train(
-        data_args=DataArgs(
+        data_args=LoaderArgs(
             logdir="artifacts/logs",
             tasks="paper-full",
             model_families=["BIG-G T=0"],
-            # model_sizes=["128b"],
+            model_sizes=["128b"],
             shots=[0],
             query_types=["multiple_choice"],
         ),
-        split="task",
+        split="instance",
         # model_name="albert-base-v2",
         # model_name_short="albert",
         # batch_size=32,
         model_name="microsoft/deberta-v3-base",
-        model_name_short="deberta",
+        model_name_short="deberta-better-split",
         batch_size=16,
         gradient_accumulation_steps=2,
+        include_model_in_input=False,
+        include_n_targets_in_input=False,
         output_dir="notebooks",
         extra_training_args={
-            "eval_steps": 2000,
-            "save_steps": 2000,
-            "warmup_steps": 6000,
-            "weight_decay": 0.01,
-            "label_smoothing_factor": 0.25,
+            #     "eval_steps": 2000,
+            #     "save_steps": 2000,
+            "warmup_steps": 3000,
+            "learning_rate": 2e-5,
+            #     "weight_decay": 0.01,
+            #     "label_smoothing_factor": 0.25,
         },
         # is_test_run=True,
     )
