38f5d3cff874fabee8d8c486e1fdb305bacd7d3e
Wout Schellaert <wout.schellaert@gmail.com>
2024-07-15 16:58:40 +0200

    Log task_accuracy reference

--- src/lass/metrics/baseline.py
+++ src/lass/metrics/baseline.py
@@ -1,3 +1,5 @@
+import logging
+
 import pandas as pd
 
 import lass.utils
@@ -15,6 +17,12 @@ def distribution_baseline(df: pd.DataFrame) -> pd.Series:
 
 
 def get_baselines(df: pd.DataFrame, metrics: list[str]) -> dict[str, dict[str, float]]:
+    # Check if multiple models present
+    if df[["model_name", "model_family"]].nunique().max() > 1:
+        logging.warning(
+            "Data contains multiple models. Baselines for all models together, i.e. as if there was only one model."
+        )
+
     # The label is for the 'failure prediction task', i.e. whether the model was correct or not.
     labels = df["correct"]
     distribution = lass.metrics.metrics.compute_metrics(
@@ -32,8 +40,11 @@ def get_baselines(df: pd.DataFrame, metrics: list[str]) -> dict[str, dict[str, f
         labels,
         metrics,
     )
+    task_accuracy = df.groupby("task")["correct"].mean()
+
     return (
         lass.utils.prefix_keys(distribution, "conf_distribution_")
         | lass.utils.prefix_keys(conf_normalized, "conf_normalized_")
         | lass.utils.prefix_keys(conf_absolute, "conf_absolute_")
+        | {"task_accuracy": task_accuracy.mean()}
     )
